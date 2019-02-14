691462a669042934d3890fef17e471ac4ad27868
Corey Trampe <corey.trampe@gmail.com>
2019-02-14 15:06:32 -0500

    add `prev` object to notify function, to observe changes in UPDATE

--- src/functionManager/createFunction.js
+++ src/functionManager/createFunction.js
@@ -3,21 +3,29 @@ const sqlCreateFunction = (channel, schema, fn) => {
         CREATE OR REPLACE FUNCTION ${schema}.${fn}() RETURNS trigger AS $$
         DECLARE
           data json;
+          prev json;
           notification json;
         BEGIN
 
-          IF (TG_OP = 'DELETE') THEN
-            data = row_to_json(OLD);
-          ELSE
-            data = row_to_json(NEW);
-          END IF;
+          CASE TG_OP
+            WHEN 'INSERT' THEN
+              data = row_to_json(NEW);
+
+            WHEN 'UPDATE' THEN
+              prev = row_to_json(OLD);
+              data = row_to_json(NEW);
+              
+            WHEN 'DELETE' THEN
+              prev = row_to_json(OLD);
+          END
 
           notification = json_build_object(
             'type', TG_OP,
             'dateTime', clock_timestamp(),
             'table', TG_TABLE_NAME::text,
             'schema', TG_TABLE_SCHEMA::text,
-            'data', data
+            'data', data,
+            'prev', prev
           );
 
           PERFORM pg_notify('${channel}', notification::text);
